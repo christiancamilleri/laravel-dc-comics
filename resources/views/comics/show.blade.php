6fadbc202f74c59e58dba84fbe478fc14f4c918a
christiancamilleri <camilleri.christian@outlook.com>
2023-05-17 13:24:15 +0200

    feat: bonus complete

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -80,12 +80,37 @@
     </div>
     <button class="btn btn-primary my-create"><a href="{{route('comics.edit', $comic->id)}}">Modifica fumetto</a> </button>
     
-    <form action="{{route('comics.destroy', $comic->id)}}" method="POST">
-    @csrf
-    @method('DELETE')
+    
 
-    <button type="submit" class="mt-3 my-create delete-btn">Elimina fumetto </button>
-    </form>
+    <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Elimina fumetto
+      </button>
+      
+      <!-- Modal -->
+      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="exampleModalLabel">Elimina fumetto</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              Sei sicuro di voler eliminare questo fumetto? L'azione è irreversibile
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+              <form action="{{route('comics.destroy', $comic->id)}}" method="POST">
+                @csrf
+                @method('DELETE')
+            
+                <button type="submit" class="btn btn-danger my-create delete-btn">Elimina fumetto </button>
+                </form>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+      
 
 </div>
 
